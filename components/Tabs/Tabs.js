2c20788253d97c27651483dd45d8d985321a7f3c
Alan Perez <alanp9six@gmail.com>
2019-03-07 16:53:38 -0600

    MVP Completed

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,7 +14,7 @@ class TabLink {
     this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click',() => { this.select() });
+    this.element.addEventListener('click',() => this.select());
   };
 
   select() {
@@ -22,7 +22,7 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(array => array.classList.remove('tabs-link-selected'));
+    Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
